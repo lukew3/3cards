282c7a453cc51728705b24bdc42f7a3a3a68d69e
lukew3 <lukew25073@gmail.com>
2022-07-11 17:37:13 -0400

    initial commit

---
+++ pages/create.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Create.module.css'
+
+const CreateTerm = (props: { id: string}) => {
+  return(
+    <div className={styles.term}>
+      <div className={styles.term_header}>
+        <div>{props.id}</div>
+        <div className={styles.term_ops}>
+          <div>↑</div>
+          <div>↓</div>
+          <div>X</div>
+        </div>
+      </div>
+      <div className={styles.term_input}>
+        <input type="text" placeholder="term"></input>
+        <input type="text" placeholder="definition"></input>
+      </div>
+    </div>
+  )
+}
+
+const Create: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Set - 3cards</title>
+        <meta name="description" content="Create a flash card set with 3cards" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <nav>
+        <h1>3cards</h1>
+      </nav>
+
+      <main className={styles.main}>
+        <h3>Create Set</h3>
+        <div className={styles.terms}>
+          <CreateTerm 
+            id="1"
+          />
+          <CreateTerm 
+            id="2"
+          />
+        </div>
+        <div className={styles.addItem} tabIndex={0}>
+          <p>Add item +</p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Create
