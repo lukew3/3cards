de8a81e277f8cccad1b6109730b39baeb17121e4
lukew3 <lukew25073@gmail.com>
2022-07-11 23:12:51 -0400

    update terms state

--- pages/create.tsx
+++ pages/create.tsx
@@ -5,20 +5,38 @@ import React, { useState } from 'react';
 import Nav from '../components/nav';
 import styles from '../styles/Create.module.css'
 
-const CreateTerm = (props: { id: number}) => {
+const CreateTerm = (props: { 
+  id: number, 
+  termPair: string[],
+  deleteTerm : (id: number) => void
+  setDefValue : (id : number, value: string) => void,
+  setTermValue : (id : number, value: string) => void,
+}) => {
   return(
     <div className={styles.term}>
       <div className={styles.term_header}>
-        <div>{props.id}</div>
+        <div>{props.id + 1}</div>
         <div className={styles.term_ops}>
           <div>↑</div>
           <div>↓</div>
-          <div>X</div>
+          <div
+            onClick={() => props.deleteTerm(props.id)}
+          >X</div>
         </div>
       </div>
       <div className={styles.term_input}>
-        <input type="text" placeholder="term"></input>
-        <input type="text" placeholder="definition"></input>
+        <input 
+          type="text" 
+          placeholder="term" 
+          onChange={(e) => props.setTermValue(props.id, e.target.value)}
+          value={props.termPair[0]}
+        />
+        <input 
+          type="text"
+          placeholder="definition"
+          onChange={(e) => props.setDefValue(props.id, e.target.value)}
+          value={props.termPair[1]}
+        />
       </div>
     </div>
   )
@@ -29,6 +47,25 @@ const Create: NextPage = () => {
 
   const addTerm = () => {
     setTerms(state => [...state, ['', '']]);
+  }
+
+  const deleteTerm = (index : number) => {
+    setTerms(state => [
+      ...state.slice(0, index),
+      ...state.slice(index + 1)
+    ]);
+  }
+
+  const setTermValue = (index : number, value : string) => {
+    let newTerms = [...terms];
+    newTerms[index][0] = value;
+    setTerms(newTerms)
+  }
+
+  const setDefValue = (index : number, value : string) => {
+    let newTerms = [...terms];
+    newTerms[index][1] = value;
+    setTerms(newTerms)
   }
 
   return (
@@ -46,8 +83,12 @@ const Create: NextPage = () => {
             terms.map((_, index) => {
               return(
                 <CreateTerm
-                  key={index+1}
-                  id={index+1}
+                  key={index}
+                  id={index}
+                  termPair={terms[index]}
+                  setTermValue={setTermValue}
+                  setDefValue={setDefValue}
+                  deleteTerm={deleteTerm}
                 />
               );
             })
