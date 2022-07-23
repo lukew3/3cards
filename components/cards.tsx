014479d14e5a9dda8934ac217be668a50cbe25f2
lukew3 <lukew25073@gmail.com>
2022-07-23 15:12:44 -0400

    Add keydown handling for cards

--- components/cards.tsx
+++ components/cards.tsx
@@ -6,20 +6,47 @@ const Cards = (props: {
 }) => {
   const termsLen = props.terms.length;
   const [termId, setTermId] = useState(0);
-  const [showingTerm, setShowingTerm] = useState(true);  
+  const [showingTerm, setShowingTerm] = useState(true);
+
+  const showPrev = () => {
+    if (termId - 1 >= 0) setTermId(termId - 1);
+    setShowingTerm(true);
+  }
+
+  const showNext = () => {
+    if (termId + 1 < termsLen) setTermId(termId + 1);
+    setShowingTerm(true);
+  }
+  
+  const toggleShowingTerm = () => {
+    setShowingTerm(!showingTerm);
+  }
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowLeft') showPrev();
+    else if (e.key === 'ArrowRight') showNext();
+    else if (e.key === 'ArrowUp') toggleShowingTerm();
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+  }, [termId, showingTerm]);
 
   return (
     <div className={styles.cards}>
       <div
         className={styles.card}
-        onClick={() => setShowingTerm(!showingTerm)}
+        onClick={toggleShowingTerm}
       >
         {showingTerm ? props.terms[termId][0] : props.terms[termId][1]}
       </div>
       <div className={styles.controls}>
-        <button onClick={() => {if (termId - 1 >= 0) setTermId(termId - 1)}}>&lt;</button>
+        <button onClick={showPrev}>&lt;</button>
         <p>{termId + 1}/{termsLen}</p>
-        <button onClick={() => {if (termId + 1 < termsLen) setTermId(termId + 1)}}>&gt;</button>
+        <button onClick={showNext}>&gt;</button>
       </div>
     </div>
   )
