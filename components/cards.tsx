ac8b349072318558f9df4bb52583a3f21a010886
Luke Weiler <lukew25073@gmail.com>
2022-07-24 17:33:51 -0400

    Add scrollable cards (#1)
    * wip
    
    * Improve cards css snapping
    
    * change width of cards
    
    * fix flip card on click
    
    * added ScrollToTerm
    
    * update termId on scroll
    
    * Make width the same as before
    
    * fix key error

--- components/cards.tsx
+++ components/cards.tsx
@@ -8,13 +8,27 @@ const Cards = (props: {
   const [termId, setTermId] = useState(0);
   const [showingTerm, setShowingTerm] = useState(true);
 
+  const scrollToTerm = (termIndex : number) => {
+    const cards_list_item = document.getElementById('Cards_cards_list');
+    if (cards_list_item) {
+      const scrollDist = cards_list_item.scrollWidth / termsLen;
+      cards_list_item.scrollTo(termIndex * scrollDist, 0);
+    }
+  }
+
   const showPrev = () => {
-    if (termId - 1 >= 0) setTermId(termId - 1);
+    if (termId - 1 >= 0) {
+      scrollToTerm(termId-1);
+      setTermId(termId - 1);
+    }
     setShowingTerm(true);
   }
 
   const showNext = () => {
-    if (termId + 1 < termsLen) setTermId(termId + 1);
+    if (termId + 1 < termsLen) {
+      scrollToTerm(termId+1);
+      setTermId(termId + 1);
+    }
     setShowingTerm(true);
   }
   
@@ -28,6 +42,12 @@ const Cards = (props: {
     else if (e.key === 'ArrowUp') toggleShowingTerm();
   }
 
+  const handleCardsListScroll = (e : any) => {
+    const scrollDist = e.target.scrollWidth / termsLen;
+    const nextTermId = Math.ceil(e.target.scrollLeft / scrollDist);
+    setTermId(nextTermId);
+  }
+
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
     return () => {
@@ -35,19 +55,40 @@ const Cards = (props: {
     }
   }, [termId, showingTerm]);
 
+  useEffect(() => {
+    const cards_list_item = document.getElementById('Cards_cards_list');
+    if (cards_list_item) {
+      handleCardsListScroll({ target: cards_list_item}); // Set term on initial load
+      window.addEventListener('keydown', handleKeyDown);
+      cards_list_item.addEventListener('scroll', handleCardsListScroll);
+      return () => {
+        cards_list_item.removeEventListener('keydown', handleKeyDown);
+      }
+    }
+  }, []);
+
   return (
     <div className={styles.cards}>
       <input type="checkbox" className={styles.show_term_checkbox} checked={!showingTerm} onChange={toggleShowingTerm} />
-      <div
-        className={styles.card}
-        onClick={toggleShowingTerm}
-      >
-        <div className={styles.card_front}>
-          {props.terms[termId][0]}
-        </div>
-        <div className={styles.card_back}>
-          {props.terms[termId][1]}
-        </div>
+      <div id="Cards_cards_list" className={styles.cards_list}>
+        {
+          props.terms.map((termPair, index) => {
+            return(
+              <div
+                className={styles.card}
+                key={`cards-card-${index}`}
+                onClick={toggleShowingTerm}
+              >
+                <div className={styles.card_front}>
+                  {termPair[0]}
+                </div>
+                <div className={styles.card_back}>
+                  {termPair[1]}
+                </div>
+              </div>
+            );
+          })
+        }
       </div>
       <div className={styles.controls}>
         <button onClick={showPrev}>&lt;</button>
